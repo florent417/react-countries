31d551fb0a26106be8d192513319c1dfa7eabb31
Florent <avdiflor@gmail.com>
2024-03-28 12:04:33 +0100

    first commit for filter by region

--- src/views/Countries.tsx
+++ src/views/Countries.tsx
@@ -20,6 +20,14 @@ const Countries = () => {
   return (
     <>
       <Header />
+      <select className="bg-white">
+        <option value="" disabled selected hidden>
+          Select your option
+        </option>
+        <option value="apple">Apple</option>
+        <option value="banana">Banana</option>
+        <option value="orange">Orange</option>
+      </select>
       <div className="flex justify-center">
         <div className="flex justify-between flex-wrap gap-x-8 gap-y-16 w-11/12">
           {countries.map((country) => (
