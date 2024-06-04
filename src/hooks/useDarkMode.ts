37fb29fadf4ec286625c15204ba84aff00584f08
Florent <avdiflor@gmail.com>
2024-06-04 22:07:51 +0200

    Added darkmode hook

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const useDarkMode = () => {
+  const [theme, setTheme] = useState<string>(localStorage.theme as string);
+
+  const colorTheme = theme === 'dark' ? 'light' : 'dark';
+
+  localStorage.setItem('theme', theme);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+    if (localStorage.theme == 'dark') localStorage.removeItem('theme');
+    else localStorage.setItem('theme', theme);
+  }, [theme, colorTheme]);
+
+  return [colorTheme, setTheme];
+};
+
+export { useDarkMode };
