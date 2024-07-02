44d338a676f5d8171d809c96328f82cb561c271c
Florent <avdiflor@gmail.com>
2024-07-02 14:15:11 +0200

    bigger text in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { DarkModeSwitch } from './DarkModeSwitch';
 
 const Header = () => (
-  <nav className="bg-white dark:bg-dark-blue h-14 mb-8 shadow-sm flex justify-center">
+  <nav className="bg-white dark:bg-dark-blue h-20 shadow-sm flex justify-center">
     <div className="h-full flex items-center justify-between w-11/12">
       <p className="text-left text-xl font-bold">Where in the world?</p>
       <DarkModeSwitch />
