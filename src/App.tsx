1c9d1bbb36f61c7b63539d5560da977a95de0d32
Florent <avdiflor@gmail.com>
2024-07-02 14:50:00 +0200

    moived color changing to body

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { Countries } from './views/Countries';
 
 function App() {
   return (
-    <div className="dark:text-white">
+    <>
       <Header />
-      <main className="flex justify-center pt-8 dark:bg-very-dark-blue-bg">
+      <main className="flex justify-center pt-8">
         <Countries />
       </main>
-    </div>
+    </>
   );
 }
 
