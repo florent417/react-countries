d0042b8c867a3566a29a9d285049cccf198be1db
Florent <avdiflor@gmail.com>
2024-07-02 13:47:58 +0200

    height on image section

--- src/components/countries/CountryCard.tsx
+++ src/components/countries/CountryCard.tsx
@@ -6,8 +6,9 @@ type Props = {
 
 const CountryCard = ({ country }: Props) => {
   return (
-    <Card className="bg-white dark:bg-dark-blue rounded-md flex-[0_0_20%] flex-shrink shadow-md pb-4 overflow-hidden">
-      <img src={country.flags.svg} />
+    <Card className="bg-white dark:bg-dark-blue rounded-md flex-[0_0_20%] flex-shrink shadow-md overflow-hidden">
+      {/* TODO: How to fit image */}
+      <img className="object-contain w-full h-1/2" src={country.flags.svg} />
       <div className="text-left p-6">
         <p className="text-xl font-bold mb-3">{country.name.common}</p>
         <div>
