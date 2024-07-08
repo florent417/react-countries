d56ebf815e37c51f4901a4b2c048a437e22d8d72
Florent <avdiflor@gmail.com>
2024-07-08 16:06:09 +0200

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Countries } from './views/Countries';
 import { Layout } from './components/ui/layout/Layout';
+import { CountryDetails } from './views/CountryDetails';
+import { countryDetailsLoader } from './loaders/CountryDetailsLoader';
 
+// TODO: How to add country details as a child of Countries list
 const router = createBrowserRouter([
   {
     path: '/',
     Component: Layout,
-    children: [{ index: true, Component: Countries }],
+    children: [
+      { index: true, Component: Countries },
+      {
+        path: 'country/:country',
+        Component: CountryDetails,
+        loader: countryDetailsLoader,
+      },
+    ],
   },
 ]);
 
