17d8014c7dce3d217c2139df1807580f4177b8f6
Florent Avdijaj <florent.avdijaj@twoday.com>
2024-11-28 12:47:57 +0100

    border countries in loader

--- src/loaders/CountryDetailsLoader.ts
+++ src/loaders/CountryDetailsLoader.ts
@@ -1,15 +1,27 @@
 import { LoaderFunctionArgs } from 'react-router-dom';
-import { getCountry } from '../services/CountriesService';
+import {
+  getCountriesByCountryCodes,
+  getCountry,
+} from '../services/CountriesService';
 import { Country } from '../models/Country';
+
+export type CountryDetailsData = {
+  countryData: Country;
+  borderCountries: Country[];
+};
 
 export const countryDetailsLoader = async ({
   params,
-}: LoaderFunctionArgs): Promise<Country> => {
+}: LoaderFunctionArgs): Promise<CountryDetailsData> => {
   if (!params.country) {
     throw new Error('Expected params.country');
   }
   const { country } = params;
-
-  return await getCountry(country);
+  const countryData = await getCountry(country);
+  let borderCountries = [] as Country[];
+  if (countryData.borders) {
+    borderCountries = await getCountriesByCountryCodes(countryData.borders);
+  }
+  return { countryData, borderCountries };
 };
 // Inspired by: https://www.dhiwise.com/post/guide-to-routing-in-react-using-useloaderdata-typescript#structuring-the-sample-route
