7f475a7824e9636af84929c8969b0d5a8625f925
Florent Avdijaj <florent.avdijaj@twoday.com>
2024-11-28 12:48:33 +0100

    refactor of details and border countries display

--- src/views/CountryDetails.tsx
+++ src/views/CountryDetails.tsx
@@ -1,48 +1,58 @@
 import { useLoaderData } from 'react-router-dom';
-import { Country } from '../models/Country';
 import { Card } from '../components/common/Card';
 import { BackButton } from '../components/common/BackButton';
+import { CountryDetailsData } from '../loaders/CountryDetailsLoader';
+import { CountryButton } from '../components/countries/CountryButton';
 
 // TODO: Should there be a check for if the country has been retrieved?
 export const CountryDetails = () => {
   // TODO: Should this be changed?
-  const country = useLoaderData() as Country;
+  const { countryData, borderCountries } =
+    useLoaderData() as CountryDetailsData;
 
-  const currencies = Object.keys(country.currencies)
-    .map((key) => country.currencies[key].name)
-    .join(', ');
-  const languages = Object.values(country.languages).sort().join(', ');
-  const topLevelDomains = country.tld.join(', ');
+  const currencies = countryData.currencies
+    ? Object.keys(countryData.currencies)
+        .map((key) => countryData.currencies[key].name)
+        .join(', ')
+    : null;
+  const languages = countryData.languages
+    ? Object.values(countryData.languages).sort().join(', ')
+    : null;
+  const topLevelDomains = countryData.tld?.join(', ');
   // TODO: Should it be done this way?
-  const nativeName =
-    country.name.nativeName[Object.keys(country.name.nativeName).pop()!].common;
+  const nativeName = countryData.name.nativeName
+    ? countryData.name.nativeName[
+        Object.keys(countryData.name.nativeName).pop()!
+      ].common
+    : countryData.name.official;
 
   return (
     <div className="flex flex-col w-11/12 items-start gap-y-16 mt-8">
       <BackButton />
-      <Card className="flex flex-grow justify-between w-full m-auto">
-        <img className="w-2/6" src={country.flags.svg} />
-        <section className="text-left p-6 m-auto w-auto">
-          <p className="text-3xl font-bold mb-6">{country!.name.common}</p>
+      <Card className="flex justify-between w-full">
+        <img className="w-5/12 h-2/3 object-fill" src={countryData.flags.svg} />
+        <section className="text-left p-6 w-5/12">
+          <p className="text-3xl font-bold mb-6">{countryData!.name.common}</p>
           <div className="flex justify-between m-auto gap-x-10">
-            <div className="flex flex-col gap-y-4">
+            <div className="flex flex-col gap-y-2">
               <p>
                 <b>Native Name:</b> {nativeName}
               </p>
               <p>
-                <b>Population:</b> {country.population.toLocaleString('EN-US')}
+                <b>Population:</b>{' '}
+                {countryData.population.toLocaleString('EN-US')}
               </p>
               <p>
-                <b>Region:</b> {country.region}
+                <b>Region:</b> {countryData.region}
               </p>
               <p>
-                <b>Sub Region:</b> {country.subregion}
+                <b>Sub Region:</b> {countryData.subregion}
               </p>
               <p>
-                <b>Capital:</b> {country.capital}
+                <b>Capital:</b> {countryData.capital}
               </p>
             </div>
-            <div className="flex flex-col gap-y-4">
+            <div className="flex flex-col gap-y-2">
               <p>
                 <b>Top Level Domain:</b> {topLevelDomains}
               </p>
@@ -54,11 +64,20 @@ export const CountryDetails = () => {
               </p>
             </div>
           </div>
+          {/* TODO */}
+          {borderCountries && (
+            <div className="flex mt-12 gap-x-2 gap-y-4 items-center flex-wrap">
+              <b className="mr-2">Border Countries:</b>
+              {borderCountries.map((borderCountry) => (
+                // TODO: FIxed size?
+                <CountryButton
+                  countryName={borderCountry.name.common}
+                  key={borderCountry.name.common}
+                />
+              ))}
+            </div>
+          )}
         </section>
-        {/* TODO */}
-        {/* <span>
-          <p><b>Border Countries:</b>{country.}</p>
-        </span> */}
       </Card>
     </div>
   );
