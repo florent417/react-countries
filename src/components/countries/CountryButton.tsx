a88c43345067de31af8b8710818f414251dafffe
Florent Avdijaj <florent.avdijaj@twoday.com>
2024-11-28 12:47:40 +0100

    Country border button

---
+++ src/components/countries/CountryButton.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
+type Props = {
+  countryName: string;
+};
+
+// TODO: Add dark theme
+export const CountryButton = ({ countryName }: Props) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/country/${countryName}`);
+  };
+
+  return (
+    // TODO: FIxed size?
+    <button type="button" onClick={handleClick}>
+      <p className="bg-white dark:bg-dark-blue rounded-sm shadow-sm px-4 py-1">
+        {countryName}
+      </p>
+    </button>
+  );
+};
