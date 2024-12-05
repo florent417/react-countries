cbc3e6fb606b06fcc2dbf8f2f29bb5f1bd847da4
Florent Avdijaj <florent.avdijaj@twoday.com>
2024-12-05 13:14:13 +0100

    back btn dark mode

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
+import { useDarkMode } from '../../hooks/useDarkMode';
 
 export const BackButton = () => {
+  const [theme] = useDarkMode();
   const navigate = useNavigate();
 
   const goBack = () => navigate(-1);
@@ -17,7 +19,17 @@ export const BackButton = () => {
         width="16"
         className="float-left mr-4"
       >
-        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z" />
+        {theme === 'dark' ? (
+          <path
+            fill="#ffffff"
+            d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+          />
+        ) : (
+          <path
+            fill="#000000"
+            d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"
+          />
+        )}
       </svg>
       <p className="inline-block text-base">Back</p>
     </button>
